c9ebb72b8c21a4a4c63c188d831a5143a804fcdb
Evan Scott <probablyup@gmail.com>
2018-10-10 12:34:04 -0500

    add a second use of injectGlobal to test something else

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { injectGlobal } from 'styled-components';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+injectGlobal`
+  html {
+    color: red;
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
